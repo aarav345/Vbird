6c2fd6d1ff6d0ef8d009c7cc3fb769b40ac0e995
aarav <aaravpradhan12365@gmail.com>
2023-09-24 12:20:09 +0545

    made some changes in the getstarted page

--- frontEnd/screens/GetStarted.js
+++ frontEnd/screens/GetStarted.js
@@ -1,34 +1,32 @@
 import React from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Text, Image, Button} from "react-native"
+
 
 export const GetStarted = () => {
   return (
     <>
       <View className="bg-[#186B23] h-full">
-        <View className="relative">
-          <View className="absolute ">
-            <Image
-              source={require("C:/Users/dell/Desktop/level 6/fyp/Vbird/frontEnd/assets/greenblock.svg")}
-              style={{ width: 269, height: 300 }}
-            />
+        <View className="grid grid-flow-row items-start">
+          <View className="absolute bg-[#438926] w-[100%] h-[50vh]">
           </View>
           <View>
             <Text className="font-bold text-4xl mt-6 ml-12 text-[#E5F4DC]">VBIRD</Text>
           </View>
-          <View className="absolute right-9 top-10">
+          <View className="relative -left-8 items-left m-0 p-0">
             <Image
               source={require("C:/Users/dell/Desktop/level 6/fyp/Vbird/frontEnd/assets/1.svg")}
-              style={{ width: 269, height: 300 }}
+              style={{ width: 300, height: 400 }}
+              className="h-[50vh]"
             />
           </View>
-          <View className="relative top-64 left-10">
+          <View className="flex items-center">
             <Text className="text-3xl w-2/3 text-[#FFFFFF]  font-medium md-4">Explore Bird Calls</Text>
             <Text className={`text-sm w-4/6 text-[#82AF88] `}>
               Uncover Bird Identities Through Their Symphonies
             </Text>
           </View>
-          <View className="bg-[#438926]">
-            <Button>let's get Started</Button>
+          <View className="w-2/3 items-center">
+            <Button title="Let's get Started" color="#438926"/>
           </View>
         </View>
       </View>
